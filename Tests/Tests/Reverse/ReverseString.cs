4c231c2ed83777ee8ba84bc77e8964130398851f
Jurie Kritzinger <jurie.kritzinger@bmwiti.onmicrosoft.com>
2021-06-22 08:37:38 +0200

    Updated

--- Tests/Tests/Reverse/ReverseString.cs
+++ Tests/Tests/Reverse/ReverseString.cs
@@ -1,5 +1,4 @@
-/*
-
+﻿/*
 ### Requirement 1
 Complete the `reverse` method on the `String` class. You are not allowed to use built-in methods. 
 
@@ -7,58 +6,58 @@ Complete the `reverse` method on the `String` class. You are not allowed to use 
 Refactor the `reverse` method from requirement 1 in order to cater for special characters. Special characters should remain in their
 place. Only non-special characters should be reversed. There is no need to implement the special character check. This has already 
 been implemented for you in the `isSpecialCharacter` method.
-
 */
 
-namespace Tests
-{
-    public class String
-    {
-        static bool isSpecialCharacter(char c)
-        {
-            return !(char.IsWhiteSpace(c) | char.IsLetterOrDigit(c));
-        }
+using NUnit.Framework;
 
-        public static string reverse(string s)
+namespace Tests.Reverse
+{
+    public class MyString
+    {
+        private static bool IsSpecialCharacter(char character) 
+            => !(char.IsWhiteSpace(character) | char.IsLetterOrDigit(character));
+
+        public static string Reverse(string input)
         {
             // Complete me
             return "";
         }
     }
 
-    public class ReverseStringTests
+    [TestFixture]
+    public class ReverseString
     {
-        [TestMethod]
+        [Test]
         public void TestRequirement1()
         {
-            var name = "James";
-            var reversedName = String.reverse(name);
+            const string name = "James";
+            var reversedName = MyString.Reverse(name);
 
             Assert.AreEqual("semaJ", reversedName);
         }
 
-        [TestMethod]
-        public void TestRequirement2a()
+        [Test]
+        public void TestRequirement2A()
         {
-            var name = "J@me$";
-            var reversedName = String.reverse(name);
+            const string name = "J@me$";
+            var reversedName = MyString.Reverse(name);
 
             Assert.AreEqual("e@mJ$", reversedName);
         }
 
-        [TestMethod]
-        public void TestRequirement2b()
+        [Test]
+        public void TestRequirement2B()
         {
-            var name = "J@m3$ 3ond!";
-            var reversedName = String.reverse(name);
+            const string name = "J@m3$ 3ond!";
+            var reversedName = MyString.Reverse(name);
             Assert.AreEqual("d@no$3 3mJ!", reversedName);
         }
 
-        [TestMethod]
-        public void TestRequirement2c()
+        [Test]
+        public void TestRequirement2C()
         {
-            var name = "J[a]me$s %Bo$nd% I^I*I";
-            var reversedName = String.reverse(name);
+            const string name = "J[a]me$s %Bo$nd% I^I*I";
+            var reversedName = MyString.Reverse(name);
             Assert.AreEqual("I[I]I $dn%oB$ s%em^a*J", reversedName);
         }
     }
