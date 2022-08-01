47615348c31328e75b50a58be7f59ef23728e8e4
Jurie Kritzinger (ext.) <Jurie.Kritzinger@partner.bmw.co.za>
2022-08-01 15:40:23 +0200

    Updated

--- Tests/Tests/Words/WordCount.cs
+++ Tests/Tests/Words/WordCount.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using Tests.Reverse;
 
 namespace Tests.Words
 {
@@ -7,7 +6,6 @@ namespace Tests.Words
     {
         public static int Count(string sentence)
         {
-            //COMPLETE ME
             return 0;
         }
     }
@@ -35,7 +33,7 @@ namespace Tests.Words
         public void Test2()
         {
             const string sentence = "He is a good programmer, he  won 865 competitions, but sometimes he dont. What do you think? All test-cases should pass. Done-done";
-            var count = MyString.Reverse(sentence);
+            var count = WordCount.Count(sentence);
 
             Assert.AreEqual(21, count);
         }
